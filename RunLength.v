db61edb321df14572b5f522cd7d9639ef3208925
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-09-24 16:08:15 -0400

    Prove run_length_cons_bounded

--- RunLength.v
+++ RunLength.v
@@ -1,6 +1,8 @@
 Require Import List.
 Import ListNotations.
 Require Import Coq.Init.Nat.
+Require Import Omega.
+Require Import Coq.Bool.Bool.
 
 Require Import VST.fcf.EqDec.
 
@@ -31,7 +33,38 @@ Section RunLength.
   Definition run_length_decode (l : list (A * nat)) :=
     List.fold_right run_length_cons_inverse [] l.
 
+  Definition run_bounded maxRun (l: list (A * nat)) :=
+    Forall (fun x => x <= maxRun) (map snd l).
+
+  Theorem run_length_cons_bounded : forall maxRun x l,
+      maxRun > 0 ->
+      run_bounded maxRun l -> run_bounded maxRun (run_length_cons maxRun x l).
+  Proof.
+    intros. destruct l.
+    - (* empty list *)
+      unfold run_bounded. simpl.
+      econstructor. omega. econstructor.
+    - (* nonempty list *)
+      destruct p. simpl.
+      destruct (EqDec_dec H x a).
+      + (* x equal to head of list *)
+        destruct (eqb_leibniz x a) as [_ L].
+        specialize (L e). rewrite L. simpl.
+        destruct (Nat.ltb_spec n maxRun).
+        * (* n < maxRun *)
+          unfold run_bounded in *. simpl.
+          econstructor. omega.
+          inversion H1. auto.
+        * (* n >= maxRun *)
+          unfold run_bounded in *. simpl.
+          repeat (econstructor; try omega; auto).
+      + (* x is new *)
+        destruct (eqb_false_iff H x a) as [_ L].
+        specialize (L n0). rewrite L. simpl.
+        repeat (econstructor; try omega; auto).
+  Qed.
+
 End RunLength.
 
-Compute run_length_encode [1; 1; 1; 2; 2; 3; 2; 1].
-Compute run_length_decode (run_length_encode [1; 1; 1; 2; 2; 3; 2; 1]).
+Compute run_length_encode 2 [1; 1; 1; 2; 2; 3; 2; 1].
+Compute run_length_decode (run_length_encode 2 [1; 1; 1; 2; 2; 3; 2; 1]).
