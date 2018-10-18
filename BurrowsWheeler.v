2b2ac370623e9b9a12167d6d54bcb011d6877915
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-10-18 02:51:39 -0400

    Define lexsort in bwp

--- BurrowsWheeler.v
+++ BurrowsWheeler.v
@@ -5,6 +5,8 @@ Require Import Recdef.
 Require Import Omega.
 
 Require Import Rot.
+Require Import Prefix.
+Require Import Ord.
 
 Section BW.
 
@@ -19,8 +21,12 @@ Fixpoint iterate_n {A: Type} (f: A -> A) (n: nat) (x: A) : list A :=
 Definition rots {A: Type} : list A -> list (list A) :=
   fun l => iterate_n lrot (length l) l.
 
-Definition lexsort {A: Type} : list (list A) -> list (list A).
-Admitted.
+Definition lexsort {A: Type} `{TotalOrderDec A} : list (list A) -> list (list A) :=
+  fun ls =>
+    match ls with
+    | [] => []
+    | hd :: tl => sort (length hd) ls
+    end.
 
 Theorem iterate_n_preserves {A: Type}: forall f n (z: A) (P: A -> Prop),
     (forall x, P x -> P (f x)) -> P z -> Forall P (iterate_n f n z).
@@ -31,10 +37,12 @@ Proof.
   - simpl. constructor; auto.
 Qed.
 
-Definition bwp {A: Type} (l: list A) : list A :=
+Definition bwp {A: Type} `{TotalOrderDec A} (l: list A) : list A :=
   match l with
   | [] => []
   | hd :: tl => (List.map (fun x => last x hd) ∘ lexsort ∘ rots) l
   end.
 
 End BW.
+
+Compute bwp [1; 2; 3].
