a67bc09aa461308597feab5488867f15a946350a
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-12-09 02:34:23 +0000

    Stash

---
+++ midpoint-presentation/default.nix
@@ -0,0 +1,48 @@
+{ lib, stdenv, callPackage, texlive, ghostscript, makeFontsConf, fira }:
+let
+  tex = texlive.combine ({
+    inherit (texlive)
+      # The Basics
+      scheme-basic xetex euenc latexmk
+      # Fonts
+      collection-fontsrecommended fontspec
+      # Bibliography
+      biblatex biblatex-ieee biber
+      # misc
+      xstring parskip etoolbox logreq titlesec
+      # Beamer
+      beamer beamertheme-metropolis translator pgfopts
+      ;
+  });
+
+  fonts = [ fira ];
+
+in stdenv.mkDerivation rec {
+  basename = "midpoint_presentation";
+
+  name = "${basename}.pdf";
+
+  buildInputs = [
+    tex
+    ghostscript
+  ];
+
+  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+  unpackPhase =
+  ''
+    cp ${./. + "/${basename}.tex"} ${basename}.tex
+  '';
+
+  buildPhase = ''
+    latexmk --xelatex ${basename}.tex -jobname=${basename}
+  '';
+
+  installPhase = ''
+    cp ${basename}.pdf $out
+  '';
+
+  FONTCONFIG_FILE = makeFontsConf { fontDirectories = fonts; };
+
+  enableParallelBuilding = true;
+}
