09664455da5cc95d5220f21aa517a6da058862f8
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-10-06 20:23:00 -0400

    Add all packages for acmart to nix

--- draft_paper/default.nix
+++ draft_paper/default.nix
@@ -2,30 +2,77 @@
 let
   tex = texlive.combine ({
     inherit (texlive)
-      # The Basics
-      scheme-basic xetex euenc latexmk
-      # Fonts
-      collection-fontsrecommended fontspec
-      # Bibliography
-      biblatex biblatex-ieee biber
-      etoolbox logreq xstring
-      # thmtools
-      thmtools epsf
-      listings
-      parskip
-      cleveref
-      mathtools
-      lkproof
-      enumitem
-      mathpazo caption microtype titlesec sourcesanspro xkeyval
-      xcolor paralist
-      xltxtra realscripts eulervm
-      ;
-    });
+    amscls
+    amsfonts
+    amsmath
+    biber
+    biblatex
+    biblatex-ieee
+    boondox
+    caption
+    cleveref
+    cm-super
+    cmap
+    collection-fontsrecommended
+    comment
+    draftwatermark
+    enumitem
+    environ
+    epsf
+    etoolbox
+    euenc
+    eulervm
+    fancyhdr
+    float
+    fontaxes
+    fontspec
+    geometry
+    graphics
+    graphics-def
+    hyperref
+    ifluatex
+    ifxetex
+    inconsolata
+    kastrup
+    latexmk
+    libertine
+    listings
+    lkproof
+    logreq
+    mathpazo
+    mathtools
+    microtype
+    mmap
+    ms
+    mweights
+    natbib
+    ncctools
+    newtx
+    oberdiek
+    paralist
+    parskip
+    realscripts
+    scheme-basic
+    setspace
+    sourcesanspro
+    textcase
+    thmtools
+    titlesec
+    totpages
+    trimspaces
+    upquote
+    url
+    xcolor
+    xetex
+    xkeyval
+    xltxtra
+    xstring
+    ;
+  });
 in compileTex {
   srcDir = ./.;
   texFile = "draft_paper.tex";
-  otherFiles = [ "draft_paper.bib" "thesis.cls" "lstlangcoq.sty" ];
+  otherFiles = [ "acmart.cls" "ACM-Reference-Format.bst" "draft_paper.bib" "thesis.cls" "lstlangcoq.sty" ];
   fonts = [ iosevka-bin ];
   inherit tex;
 }
